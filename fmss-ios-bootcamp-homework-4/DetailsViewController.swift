4ab232758182b042700751b5752c0059b3c7bca1
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-30 09:45:33 +0300

    Add details screen

---
+++ fmss-ios-bootcamp-homework-4/DetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsViewController.swift
+//  fmss-ios-bootcamp-homework-4
+//
+//  Created by Samet Koyuncu on 30.09.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    static let storyboardID = "DetailsVC"
+    
+    var detailsType: DetailsTypeEnum?
+
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let detailsType = detailsType {
+            titleLabel.text = detailsType.rawValue
+        }
+
+    }
+    @IBAction func bavkButtonPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func bookmarkButtonPressed(_ sender: UIButton) {
+    }
+}
